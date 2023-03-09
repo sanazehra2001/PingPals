9f28699ed65830660b483f317f289443c8a6f447
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2023-03-10 00:32:40 +0500

    Initializations

---
+++ PingPals/src/main/java/com/pingpals/PingPals/PingPalsApplication.java
@@ -0,0 +1,13 @@
+package com.pingpals.PingPals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingPalsApplication.class, args);
+	}
+
+}
